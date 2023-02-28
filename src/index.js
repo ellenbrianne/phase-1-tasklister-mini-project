f48ce82cb7d91c546ca3baa294f28cd418c836a8
Ellen Bennett <ellenbennett@Ellens-MacBook-Air.local>
2023-02-27 19:37:52 -0700

    passing lab w/minimal deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  let form = document.getElementById('create-task-form');
+
+  form.addEventListener('submit', (e) => {
+
+    e.preventDefault();
+
+    let input = form.querySelector('#new-task-description');
+    let list = document.createElement('li')
+    document.querySelector('#tasks').appendChild(list)
+    list.textContent = input.value
+
+    form.reset();
+
+  });
+
 });
